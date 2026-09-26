0e30d36714238c823a66c43a32da61a1f19bb85c
georgemoralis <giorgosmrls@gmail.com>
2026-09-26 22:57:46 +0300

    adding p2p keyring

---
+++ src/core/net/p2p_tcp.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <array>
+#include <chrono>
+#include <deque>
+#include <functional>
+#include <map>
+#include <optional>
+#include <span>
+#include <vector>
+
+#include "core/net/host_net.h"
+
+namespace Core::Net::P2P {} // namespace Core::Net::P2P
